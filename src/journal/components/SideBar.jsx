f5257f7aecaa932059e95a2e2831c931488f748f
tobiaszavallo <tobi-zavallo@hotmail.com>
2022-11-24 18:29:14 -0300

    Layout JournalApp OK

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,58 @@
+import { TurnedInNot } from "@mui/icons-material";
+import { Box, 
+        Divider, 
+        Drawer, 
+        Grid, List, 
+        ListItem, 
+        ListItemButton, 
+        ListItemIcon, 
+        ListItemText, 
+        Toolbar, 
+        Typography 
+    } from "@mui/material";
+
+
+export const SideBar = ({ drawerWidth = 240 }) => {
+  return (
+    <Box
+        component='nav'
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+        <Drawer
+            variant="permanent"   //temporary
+            open={ true }
+            sx={{ 
+                display: { xs: 'block' },
+                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+             }}
+        >   
+
+            <Toolbar>
+                <Typography variant="h6" noWrap component='div'>
+                    Tobias Zavallo
+                </Typography>
+            </Toolbar>
+            <Divider />
+
+            <List>
+                {
+                    ['Enero', 'Febrero', 'Marzo', 'Abril'].map(text => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <TurnedInNot />
+                                </ListItemIcon>
+                                <Grid container>
+                                    <ListItemText primary={text}/>
+                                    <ListItemText secondary={'lorem lsladkf sdfjkfd jasdfh '}/>
+                                </Grid>
+                            </ListItemButton>
+                        </ListItem>
+                    ))
+                }
+            </List>
+
+        </Drawer>
+    </Box>
+  )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,18 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      
+      <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio quaerat pariatur reprehenderit? In quis, dolorem itaque accusantium sapiente aut nemo autem, non error tempora quidem, cupiditate voluptatibus dicta rem! Unde.</Typography>
+
+
+      {/* NothingSelected */}
+      {/* NoteView */}
+
+    </JournalLayout>
   )
 }
