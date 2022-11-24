3df1551dea5ba82fc4c675593b7b4ef2ba925a5a
tobiaszavallo <tobi-zavallo@hotmail.com>
2022-11-24 19:44:50 -0300

    total layout OK

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,7 @@
-import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
 
 
 
@@ -7,11 +9,25 @@ export const JournalPage = () => {
   return (
     <JournalLayout>
       
-      <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio quaerat pariatur reprehenderit? In quis, dolorem itaque accusantium sapiente aut nemo autem, non error tempora quidem, cupiditate voluptatibus dicta rem! Unde.</Typography>
+      {/*<Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio quaerat pariatur reprehenderit? In quis, dolorem itaque accusantium sapiente aut nemo autem, non error tempora quidem, cupiditate voluptatibus dicta rem! Unde.</Typography> */}
+
+      <NothingSelectedView /> 
+      {/* <NoteView /> */}
 
 
-      {/* NothingSelected */}
-      {/* NoteView */}
+      <IconButton 
+        size='larg'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }} 
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
 
     </JournalLayout>
   )
